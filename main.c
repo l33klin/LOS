88791097450997f10dd81e56ddc16fbd558a13d2
Jann <l33klin@foxmail.com>
2024-04-16 23:12:15 +0800

    hello

---
+++ main.c
@@ -0,0 +1,24 @@
+//彭东 @ 2021.01.09
+#include "vgastr.h"
+void main()
+{
+  printf("Hello OS!");
+  return;
+}
+
+void _strwrite(char* string)
+{
+  char* p_strdst = (char*)(0xb8000);//指向显存的开始地址
+  while (*string)
+  {
+    *p_strdst = *string++;
+    p_strdst += 2;
+  }
+  return;
+}
+
+void printf(char* fmt, ...)
+{
+  _strwrite(fmt);
+  return;
+}
